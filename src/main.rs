55b6fe4be09340c6d05f70329d9090882e06f70e
maxplayerone <antekmajner@gmail.com>
2023-03-05 19:47:46 +0100

    Rotating the player while jumping

--- src/main.rs
+++ src/main.rs
@@ -19,9 +19,11 @@ struct PlayerMarker;
 struct Jump{
     value: f32,
     is_jumping: bool,
+    rotation_value: f32,
 }
 
-const PLAYER_SPEED: f32 = 100.0;
+const PLAYER_SPEED: f32 = 300.0;
+const PLAYER_ROTATION_SPEED: f32 = 5.0;
 
 fn main() {
     App::new()
@@ -34,6 +36,7 @@ fn main() {
         .add_event::<MouseWheel>()
         .add_system(player_movement_linear)
         .add_system(player_movement_jump)
+        .add_system(player_jump_animation.before(reset_player_jump))
         .add_system(reset_player_jump)
         .add_system(display_events)
         .add_system(camera_movement)
@@ -59,7 +62,7 @@ fn setup(mut commands: Commands) {
         })
         .insert(Transform {
             translation: Vec3::new(0.0, -325.0, 0.0),
-            scale: Vec3::new(1500.0, 70.0, 1.0),
+            scale: Vec3::new(5000.0, 70.0, 1.0),
             ..default()
         })
         .insert(RigidBody::Fixed)
@@ -83,13 +86,13 @@ fn setup(mut commands: Commands) {
         })
         .insert(RigidBody::Dynamic)
         .insert(LockedAxes::ROTATION_LOCKED)
-        .insert(GravityScale(10.0))
+        .insert(GravityScale(30.0))
         .insert(Velocity {
             linvel: Vec2::new(10.0, 0.0),
             angvel: 0.0,
         })
         .insert(Collider::cuboid(0.5, 0.5))
-        .insert(Jump {value: 400.0, is_jumping: false})
+        .insert(Jump {value: 800.0, is_jumping: true, rotation_value: 0.0})
         .insert(PlayerMarker)
         .insert(Name::new("Player"));
 }
@@ -111,24 +114,48 @@ fn player_movement_jump(
     keys: Res<Input<KeyCode>>,
 ){
     for (mut jump, mut velocity) in player_query.iter_mut(){
-        if keys.just_pressed(KeyCode::Up) && !jump.is_jumping{
+
+        if keys.pressed(KeyCode::Up) && !jump.is_jumping{
+
             velocity.linvel = Vec2::new(0.0, jump.value).into();
             jump.is_jumping = true;
+            println!("jumping");
         }
     }
 }
 
+fn player_jump_animation(
+    mut player_query: Query<(&mut Jump, &mut Transform), With<PlayerMarker>>,
+    time: Res<Time>,
+){
+    let (mut jump, mut transform) = player_query.single_mut();
+
+        if jump.is_jumping{
+            let rotation_value = 45.0 * time.delta_seconds() * PLAYER_ROTATION_SPEED * -1.0;
+            jump.rotation_value += rotation_value;
+            transform.rotation =  Quat::from_rotation_z(jump.rotation_value * 3.1415 / 180.0 as f32);
+        }
+}
+
+fn ceil_to_full_rotation(rotation_value: f32) -> f32{
+    let mut inum = rotation_value as i32;
+    inum = inum - 90 - (inum % 90);
+    let fnum = inum as f32;
+    fnum
+}
+
 fn reset_player_jump(
-    mut player_query: Query<(Entity, &mut Jump), (With<PlayerMarker>, Without<GroundMarker>)>,
+    mut player_query: Query<(Entity, &mut Jump, &mut Transform), (With<PlayerMarker>, Without<GroundMarker>)>,
     mut ground_query: Query<Entity, With<GroundMarker>>,
     rapier_context: Res<RapierContext>,
 ){
-    let (player_id, mut jump) = player_query.single_mut();
+    let (player_id, mut jump, mut transform) = player_query.single_mut();
 
     let ground_id = ground_query.single_mut();
 
-    if let Some(contact_pair) = rapier_context.contact_pair(player_id, ground_id) {
-       jump.is_jumping = false;
+    if let Some(_contact_pair) = rapier_context.contact_pair(player_id, ground_id) {
+        jump.is_jumping = false;
+        transform.rotation =  Quat::from_rotation_z(ceil_to_full_rotation(jump.rotation_value) * 3.1415 / 180.0 as f32);
     }
 }
 
