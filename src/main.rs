96c496fb8c2488d208c783bf98e5b1b133fc0852
maxplayerone <antekmajner@gmail.com>
2023-03-08 18:42:25 +0100

    added spriteSheets and camera movement

--- src/main.rs
+++ src/main.rs
@@ -5,36 +5,115 @@ use bevy_rapier2d::prelude::*;
 mod player;
 use player::PlayerPlugin;
 
-//other
-const BG_COLOR: Color = Color::rgb(0.2, 0.36, 0.89);
+//markers
 #[derive(Component)]
 struct GroundMarker;
-
 #[derive(Component)]
 struct CameraMarker;
+#[derive(Component)]
+struct SpikeMarker;
+
+//others
+const BG_COLOR: Color = Color::rgb(0.2, 0.36, 0.89);
 const CAMERA_ZOOM_SPEED: f32 = 50.0;
+
+#[derive(Resource)]
+struct GameAssets{
+    texture_atlas: Handle<TextureAtlas>,
+}
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest())) 
         .add_plugin(WorldInspectorPlugin)
         .add_plugin(RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(1.0))
         .add_plugin(RapierDebugRenderPlugin::default())
         .add_plugin(PlayerPlugin)
         .insert_resource(ClearColor(BG_COLOR))
+        .add_startup_system_to_stage(StartupStage::PreStartup, asset_loading)
         .add_startup_system(setup)
         .add_event::<MouseWheel>()
         .add_system(camera_movement)
+        .add_system(camera_zoom)
         .add_system(bevy::window::close_on_esc)
         .run();
 }
 
-fn setup(mut commands: Commands) {
+fn asset_loading(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut texture_atlases: ResMut<Assets<TextureAtlas>>,
+){
+    let texture_handle = asset_server.load("gj_sheet0.png");
+    let texture_atlas = TextureAtlas::from_grid(texture_handle, Vec2::new(32.0, 32.0), 8, 2, None, None);
+    let texture_atlas_handle = texture_atlases.add(texture_atlas);
+
+    commands.insert_resource(GameAssets {
+       texture_atlas: texture_atlas_handle,
+    });
+}
+
+fn setup(
+    mut commands: Commands, 
+    game_assets: Res<GameAssets>,
+) {
     commands.spawn((
-        Camera2dBundle::default(),
+        Camera2dBundle{
+            transform:  Transform{
+                translation: Vec3::new(-200.0, 0.0, 0.0),
+                ..default()
+            },
+            ..default()
+        },
         CameraMarker,
         Name::new("Camera2D"),
     ));
+
+    //spikes  
+    
+    commands.spawn(SpriteSheetBundle {
+        texture_atlas: game_assets.texture_atlas.clone(),
+        sprite: TextureAtlasSprite::new(8),
+        ..default()
+    })
+    .insert(Transform {
+        translation: Vec3::new(0.0, -260.0, 0.0),
+        scale: Vec3::new(2.0, 2.0, 1.0),
+        ..default()
+    })  
+    .insert(SpikeMarker)
+    .insert(Collider::cuboid(4.5, 10.0))
+    .insert(Name::new("Spike01"));
+
+    
+    commands.spawn(SpriteSheetBundle {
+        texture_atlas: game_assets.texture_atlas.clone(),
+        sprite: TextureAtlasSprite::new(8),
+        ..default()
+    })
+    .insert(Transform {
+        translation: Vec3::new(64.0, -260.0, 0.0),
+        scale: Vec3::new(2.0, 2.0, 1.0),
+        ..default()
+    })  
+    .insert(SpikeMarker)
+    .insert(Collider::cuboid(4.5, 10.0))
+    .insert(Name::new("Spike02"));
+
+    commands.spawn(SpriteSheetBundle {
+        texture_atlas: game_assets.texture_atlas.clone(),
+        sprite: TextureAtlasSprite::new(8),
+        ..default()
+    })
+    .insert(Transform {
+        translation: Vec3::new(128.0, -260.0, 0.0),
+        scale: Vec3::new(2.0, 2.0, 1.0),
+        ..default()
+    })  
+    .insert(SpikeMarker)
+    .insert(Collider::cuboid(4.5, 10.0))
+    .insert(Name::new("Spike03"));
+
 
     //ground
     commands
@@ -51,27 +130,49 @@ fn setup(mut commands: Commands) {
             ..default()
         })
         .insert(RigidBody::Fixed)
-        .insert(Collider::cuboid(0.5, 0.5))   
+        .insert(Collider::cuboid(0.5, 0.5))
         .insert(GroundMarker)
-        .insert(Name::new("Ground"));   
+        .insert(Name::new("Ground"));
 }
 
-fn camera_movement(
+fn camera_zoom(
     mut camera_query: Query<&mut Transform, With<CameraMarker>>,
     mut scroll_ev: EventReader<MouseWheel>,
-    keys: Res<Input<KeyCode>>,
     time: Res<Time>,
 ) {
     let mut transform = camera_query.single_mut();
     for ev in scroll_ev.iter() {
         match ev.unit {
             MouseScrollUnit::Line => {
-                if keys.pressed(KeyCode::Key1){
                     transform.scale.x -= time.delta_seconds() * CAMERA_ZOOM_SPEED * ev.y;
                     transform.scale.y -= time.delta_seconds() * CAMERA_ZOOM_SPEED * ev.y;
-                }
             }
             MouseScrollUnit::Pixel => println!("jfsalkjflksadjfklasjfklasdjfklda"),
         }
+    }
+}
+
+fn camera_movement(
+    keyboard: Res<Input<KeyCode>>,
+    mut camera_query: Query<&mut Transform, With<CameraMarker>>,
+    time: Res<Time>,
+) {
+    let mut camera = camera_query.single_mut();
+    let mut left = camera.left();
+    left = left.normalize();
+
+    let speed = 1000.0;
+
+    if keyboard.pressed(KeyCode::A) {
+        camera.translation += left * time.delta_seconds() * speed;
+    }
+    if keyboard.pressed(KeyCode::D) {
+        camera.translation -= left * time.delta_seconds() * speed;
+    }
+    if keyboard.pressed(KeyCode::S) {
+        camera.translation -= Vec3::Y * time.delta_seconds() * speed;
+    }
+    if keyboard.pressed(KeyCode::W) {
+        camera.translation += Vec3::Y * time.delta_seconds() * speed;
     }
 }
--- src/player.rs
+++ src/player.rs
@@ -1,74 +1,103 @@
 use bevy::prelude::*;
 use bevy_rapier2d::prelude::*;
 
-//player
+use crate::CameraMarker;
+use crate::GroundMarker;
+use crate::GameAssets;
+use crate::SpikeMarker;
+
 #[derive(Component)]
 struct PlayerMarker;
 
 #[derive(Component)]
-struct Jump{
+struct Jump {
     value: f32,
     is_jumping: bool,
     rotation_value: f32,
 }
 
-const PLAYER_SPEED: f32 = 300.0;
+const PLAYER_SPEED: f32 = 450.0;
 const PLAYER_ROTATION_SPEED: f32 = 5.0;
+const PLAYER_JUMP_VALUE: f32 = 900.0;
 
-use crate::CameraMarker;
-use crate::GroundMarker;
+const STARTING_POSTION: Vec3 = Vec3::new(-600.0, -220.0, 0.0);
+
+//flag for freezing player movement
+//(used for debugging)
+#[derive(Resource)]
+struct FreezeGame {
+    freeze: bool,
+}
 
 fn player_spawn(
-    mut commands: Commands
+    mut commands: Commands,
+    game_assets: Res<GameAssets>,
+) {
+    commands
+        .spawn(SpriteSheetBundle {
+            texture_atlas: game_assets.texture_atlas.clone(),
+            sprite: TextureAtlasSprite::new(0),
+            ..default()
+        })
+        .insert(Transform {
+            translation: STARTING_POSTION,
+            scale: Vec3::new(2.0, 2.0, 1.0),
+            ..default()
+        })
+        .insert(RigidBody::Dynamic)
+        .insert(LockedAxes::ROTATION_LOCKED)
+        .insert(GravityScale(31.0))
+        .insert(Velocity {
+            linvel: Vec2::new(10.0, 0.0),
+            angvel: 0.0,
+        })
+        .insert(Collider::cuboid(13.0, 13.0))
+        .insert(Jump {
+            value: PLAYER_JUMP_VALUE,
+            is_jumping: true,
+            rotation_value: 0.0,
+        })
+        .insert(PlayerMarker)
+        .insert(Name::new("Player"));
+
+    commands.insert_resource(FreezeGame { freeze: false });
+}
+
+fn toggle_player_movement_state(
+    keys: Res<Input<KeyCode>>,
+    mut freeze: ResMut<FreezeGame>,
 ){
-     commands
-     .spawn(SpriteBundle {
-         sprite: Sprite {
-             color: Color::rgb(0.49, 1.0, 0.52),
-             ..default()
-         },
-         ..default()
-     })
-     .insert(Transform {
-         translation: Vec3::new(-320.0, -220.0, 0.0),
-         scale: Vec3::new(50.0, 50.0, 1.0),
-         ..default()
-     })
-     .insert(RigidBody::Dynamic)
-     .insert(LockedAxes::ROTATION_LOCKED)
-     .insert(GravityScale(30.0))
-     .insert(Velocity {
-         linvel: Vec2::new(10.0, 0.0),
-         angvel: 0.0,
-     })
-     .insert(Collider::cuboid(0.5, 0.5))
-     .insert(Jump {value: 800.0, is_jumping: true, rotation_value: 0.0})
-     .insert(PlayerMarker)
-     .insert(Name::new("Player"));
+    if keys.pressed(KeyCode::Key2) {
+        freeze.freeze = !freeze.freeze;
+    }
 }
 
 fn player_movement_linear(
     mut player_query: Query<&mut Transform, (With<PlayerMarker>, Without<CameraMarker>)>,
     mut camera_query: Query<&mut Transform, With<CameraMarker>>,
     time: Res<Time>,
+    freeze: Res<FreezeGame>,
 ) {
-    let mut transform = player_query.single_mut();
-    let mut camera_transform = camera_query.single_mut();
+    if !freeze.freeze {
+        let mut transform = player_query.single_mut();
+        let mut camera_transform = camera_query.single_mut();
 
-    transform.translation.x += PLAYER_SPEED * time.delta_seconds();
-    camera_transform.translation.x += PLAYER_SPEED * time.delta_seconds();
+        transform.translation.x += PLAYER_SPEED * time.delta_seconds();
+        camera_transform.translation.x += PLAYER_SPEED * time.delta_seconds();
+    }
 }
 
 fn player_movement_jump(
     mut player_query: Query<(&mut Jump, &mut Velocity), With<PlayerMarker>>,
     keys: Res<Input<KeyCode>>,
-){
-    for (mut jump, mut velocity) in player_query.iter_mut(){
-
-        if keys.pressed(KeyCode::Up) && !jump.is_jumping{
-
-            velocity.linvel = Vec2::new(0.0, jump.value).into();
-            jump.is_jumping = true;
+    freeze: Res<FreezeGame>,
+) {
+    if !freeze.freeze {
+        for (mut jump, mut velocity) in player_query.iter_mut() {
+            if keys.pressed(KeyCode::Up) && !jump.is_jumping {
+                velocity.linvel = Vec2::new(0.0, jump.value).into();
+                jump.is_jumping = true;
+            }
         }
     }
 }
@@ -76,17 +105,17 @@ fn player_movement_jump(
 fn player_jump_animation(
     mut player_query: Query<(&mut Jump, &mut Transform), With<PlayerMarker>>,
     time: Res<Time>,
-){
+) {
     let (mut jump, mut transform) = player_query.single_mut();
 
-        if jump.is_jumping{
-            let rotation_value = 45.0 * time.delta_seconds() * PLAYER_ROTATION_SPEED * -1.0;
-            jump.rotation_value += rotation_value;
-            transform.rotation =  Quat::from_rotation_z(jump.rotation_value * 3.1415 / 180.0 as f32);
-        }
+    if jump.is_jumping {
+        let rotation_value = 45.0 * time.delta_seconds() * PLAYER_ROTATION_SPEED * -1.0;
+        jump.rotation_value += rotation_value;
+        transform.rotation = Quat::from_rotation_z(jump.rotation_value * 3.1415 / 180.0 as f32);
+    }
 }
 
-fn ceil_to_full_rotation(rotation_value: f32) -> f32{
+fn ceil_to_full_rotation(rotation_value: f32) -> f32 {
     let mut inum = rotation_value as i32;
     inum = inum - 90 - (inum % 90);
     let fnum = inum as f32;
@@ -94,28 +123,48 @@ fn ceil_to_full_rotation(rotation_value: f32) -> f32{
 }
 
 fn reset_player_jump(
-    mut player_query: Query<(Entity, &mut Jump, &mut Transform), (With<PlayerMarker>, Without<GroundMarker>)>,
+    mut player_query: Query<
+        (Entity, &mut Jump, &mut Transform),
+        (With<PlayerMarker>, Without<GroundMarker>),
+    >,
     mut ground_query: Query<Entity, With<GroundMarker>>,
     rapier_context: Res<RapierContext>,
-){
+) {
     let (player_id, mut jump, mut transform) = player_query.single_mut();
 
     let ground_id = ground_query.single_mut();
 
     if let Some(_contact_pair) = rapier_context.contact_pair(player_id, ground_id) {
         jump.is_jumping = false;
-        transform.rotation =  Quat::from_rotation_z(ceil_to_full_rotation(jump.rotation_value) * 3.1415 / 180.0 as f32);
+        transform.rotation = Quat::from_rotation_z(
+            ceil_to_full_rotation(jump.rotation_value) * 3.1415 / 180.0 as f32,
+        );
+    }
+}
+
+fn player_death(
+    mut player_query: Query<(Entity, &mut Transform), (With<PlayerMarker>, Without<SpikeMarker>)>,
+    mut spike_queries: Query<Entity, With<SpikeMarker>>,
+    rapier_context: Res<RapierContext>,
+){
+    let (player_id, mut transform) = player_query.single_mut();
+    
+    for spike_id in spike_queries.iter_mut(){
+        if let Some(_contact_pair) = rapier_context.contact_pair(player_id, spike_id){
+            println!("Death");
+        }
     }
 }
 
 pub struct PlayerPlugin;
-impl Plugin for PlayerPlugin{
-    fn build(&self, app: &mut App){
-        app
-        .add_startup_system(player_spawn)
-        .add_system(player_movement_linear)
-        .add_system(player_movement_jump)
-        .add_system(player_jump_animation.before(reset_player_jump))
-        .add_system(reset_player_jump);
+impl Plugin for PlayerPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_startup_system(player_spawn)
+            .add_system(player_movement_linear)
+            .add_system(player_movement_jump)
+            .add_system(player_jump_animation.before(reset_player_jump))
+            .add_system(reset_player_jump)
+            .add_system(player_death)
+            .add_system(toggle_player_movement_state);
     }
 }
